fd4b77bfa8e2d2c0fbc2c228ae0287e5e1da40c2
Natasha Kerensikova <natacha@instinctive.eu>
2016-08-05 17:25:05 +0000

    Add actual support for uploaded data signature

--- src/js/app.js
+++ src/js/app.js
@@ -23,14 +23,29 @@ const settings = {  /* "name in local storage": "form input parameter" */
 var cfg_endpoint = null;
 var cfg_data_field = null;
 var cfg_extra_fields = [];
+var cfg_sign_algo = "";
+var cfg_sign_field = "";
+var cfg_sign_field_format = "";
+var cfg_sign_key = "";
+var cfg_sign_key_format = "";
 
 var to_send = [];
 var senders = [new XMLHttpRequest(), new XMLHttpRequest()];
 var i_sender = 1;
+var jsSHA = require("/src/js/sha.js");
 
 function sendPayload(payload) {
    var data = new FormData();
    data.append(cfg_data_field, payload);
+
+   if (cfg_sign_field) {
+      console.log("key is \"" + cfg_sign_key + "\"");
+      console.log("key format is \"" + cfg_sign_key_format + "\"");
+      var sha = new jsSHA(cfg_sign_algo, "TEXT");
+      sha.setHMACKey(cfg_sign_key, cfg_sign_key_format);
+      sha.update(payload);
+      data.append(cfg_sign_field, sha.getHMAC(cfg_sign_field_format));
+   }
 
    if (cfg_extra_fields.length > 0) {
       for (var i = 0; i < cfg_extra_fields.length; i += 1) {
@@ -92,6 +107,14 @@ function encodeStored(names) {
       result += "&extra=" + cfg_extra_fields.join(",");
    }
 
+   if (cfg_sign_field) {
+      result += "&s_algo=" + encodeURIComponent(cfg_sign_algo)
+       + "&s_field=" + encodeURIComponent(cfg_sign_field)
+       + "&s_fieldf=" + encodeURIComponent(cfg_sign_field_format)
+       + "&s_key=" + encodeURIComponent(cfg_sign_key)
+       + "&s_keyf=" + encodeURIComponent(cfg_sign_key_format);
+   }
+
    console.log("Encoded from storage: " + result);
    return result;
 }
@@ -105,6 +128,11 @@ Pebble.addEventListener("ready", function() {
 
    cfg_endpoint = localStorage.getItem("url");
    cfg_data_field = localStorage.getItem("data-field");
+   cfg_sign_algo = localStorage.getItem("cfgSignAlgorithm");
+   cfg_sign_field = localStorage.getItem("cfgSignFieldName");
+   cfg_sign_field_format = localStorage.getItem("cfgSignFieldFormat");
+   cfg_sign_key = localStorage.getItem("cfgSignKey");
+   cfg_sign_key_format = localStorage.getItem("cfgSignKeyFormat");
 
    if (to_send.length >= 1) {
       sendHead();
@@ -138,6 +166,31 @@ Pebble.addEventListener("webviewclosed", function(e) {
    if (configData.url) {
       cfg_endpoint = decodeURIComponent(configData.url);
       localStorage.setItem("url", cfg_endpoint);
+   }
+
+   if (configData.signAlgorithm) {
+      cfg_sign_algo = configData.signAlgorithm;
+      localStorage.setItem("cfgSignAlgorithm", cfg_sign_algo);
+   }
+
+   if (configData.signFieldName) {
+      cfg_sign_field = configData.signFieldName;
+      localStorage.setItem("cfgSignFieldName", cfg_sign_field);
+   }
+
+   if (configData.signFieldFormat) {
+      cfg_sign_field_format = configData.signFieldFormat;
+      localStorage.setItem("cfgSignFieldFormat", cfg_sign_field_format);
+   }
+
+   if (configData.signKey) {
+      cfg_sign_key = decodeURIComponent(configData.signKey);
+      localStorage.setItem("cfgSignKey", cfg_sign_key);
+   }
+
+   if (configData.signKeyFormat) {
+      cfg_sign_key_format = configData.signKeyFormat;
+      localStorage.setItem("cfgSignKeyFormat", cfg_sign_key_format);
    }
 
    const eventArray = configData["event-list"] !== "" ? configData["event-list"].split(",") : [];
